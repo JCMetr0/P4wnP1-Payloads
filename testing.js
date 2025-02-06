d747b93909d1c68e610d178df1b1b9cdae93090f
JCMetr0 <jakub.adamczyk22@bathspa.ac.uk>
2025-02-06 02:43:41 +0000

    Updating
    Commented and Adjusted the layout of functions in order to make it more universal
    
    Should not be doing this rn. Its 2.43 AM... Help

--- testing.js
+++ testing.js
@@ -1,24 +1,30 @@
 layout("gb"); //Define keybaord layout
 
-function tsFast(){
-  typingSpeed(0,0); //Nearly instant
+function tsFast(){//Fast Typing Speed
+  typingSpeed(0,0);
 }
 
-function tsNatural(){
-  typingSpeed(100,150); //More Natural typing speed
+function tsNatural(){//Natural Typing Speed
+  typingSpeed(100,150);
 }
 
-function launchAsAdmin() {
+function launchAsAdmin(program){
     press("GUI");
     delay(200);
-    type("cmd"); //Program you want to type out or run
-    delay(500); 
+    type(program)//Program what you want to run
+    delay(1500); 
     press("CTRL SHIFT ENTER"); //Run as admin
-    delay(500);
+    delay(750);
     press("TAB"); press("TAB"); //Tabs to Yes, No check if admin run is password protected. Double tab since starts at no. WORKS WIN11
-    press("ENTER");
+    delay(500);
+  	press("ENTER");
   	delay(500);
   }
 
-tsFast();
-launchAsAdmin();
+function payload(){
+  launchAsAdmin("cmd"); //launches program entered in brackets in admin mode.
+}
+
+//Calling Functions
+tsFast(); 
+payload();
