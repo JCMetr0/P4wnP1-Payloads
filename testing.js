9dc11c996d405c4428493f9ad532baf7178e4e5f
JCMetr0 <jakub.adamczyk22@bathspa.ac.uk>
2025-02-06 02:13:50 +0000

    Added Functions + Comments
    Added basic functions with couple adjusted timings on commands.
    
    Commented Code
    
    Need to test on slower computers plus windows 10 OS and linux

--- testing.js
+++ testing.js
@@ -1,11 +1,23 @@
-ps = "powershell.exe"
-ps_wow64='%SystemRoot%\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe'
+layout("gb"); //Define keybaord layout
 
-layout("us");
-typingSpeed(0,0);
-
-function startPS(){
-    press("GUI r");
-    delay(500);
-    type("powershell\n")
+function tsFast(){
+  typingSpeed(0,0); //Nearly instant
 }
+
+function tsNatural(){
+  typingSpeed(100,150); //More Natural typing speed
+}
+
+function launchAsAdmin() {
+    press("GUI");
+    delay(200);
+    delay(500); 
+    press("CTRL SHIFT ENTER"); //Run as admin
+    delay(500);
+    press("TAB"); press("TAB"); //Tabs to Yes, No check if admin run is password protected. Double tab since starts at no. WORKS WIN11
+    press("ENTER");
+  	delay(500);
+  }
+
+tsFast();
+launchAsAdmin();
